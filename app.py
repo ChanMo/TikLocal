8c9d16aa7faf930598f47d763e7f53a1731b020b
dyou <chen.orange@aliyun.com>
2023-10-08 23:51:31 +0800

    update index.html, add dark theme, add tiktok style

--- app.py
+++ app.py
@@ -2,18 +2,20 @@ import os
 import io
 import mimetypes
 from pathlib import Path
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template, send_from_directory, request, session
 # from PIL import Image
 
 app = Flask(__name__)
+app.secret_key = b'8af3e391e6cbba8c812a6d3942b12f758a3'
 
 media_folder = Path(os.environ.get('FLASK_MEDIA', '.'))
 
 @app.route('/')
 def index():
+    theme = request.args.get('theme', session.get('theme', 'light'))
+    session['theme'] = theme
     subdir = request.args.get('subdir', '')
     directory = media_folder / subdir
-    print(directory)
     media_type = request.args.get('media_type', 'image')
     files = os.scandir(directory)
     directories = []
@@ -35,12 +37,35 @@ def index():
 
     return render_template(
         'index.html',
+        theme=theme,
         directories=directories,
         recent=files,
         media_type = media_type,
         subdir = subdir,
         subdirs = subdir.split('/')
     )
+
+
+@app.route('/tiktok')
+def tiktok():
+    theme = request.args.get('theme', session.get('theme', 'light'))
+    session['theme'] = theme
+    files = os.scandir(media_folder)
+    res = sorted(files, key=lambda row:row.stat().st_mtime, reverse=True)
+    files = []
+    for file in res:
+        if os.path.isfile(os.path.join(media_folder, file)):
+            file_extension = os.path.splitext(file)[-1]
+            mime_type = mimetypes.guess_type(file)[0]
+            if mime_type and mime_type.startswith('video'):
+                files.append(file)
+
+    return render_template(
+        'tiktok.html',
+        theme=theme,
+        files=files
+    )
+
 
 
 @app.route("/media/<name>")
